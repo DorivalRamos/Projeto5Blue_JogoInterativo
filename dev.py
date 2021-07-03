cef690ce8a9d2a508ffd44b9af9555eb6a3429c5
DorivalRamos <dorival.millan@bandtec.com.br>
2021-07-03 15:36:21 -0300

    Criação das Setter na pagina dev

--- dev.py
+++ dev.py
@@ -1,6 +1,23 @@
-class Dev: #Criação da classe para personagem principal
-    def __init__(self, nome, estamina = 12, dinheiro = 10):
-        self.nome = nome
+class Dev:  # Criação da classe para personagem principal
+    def __init__(self, nome, estamina=12, dinheiro=10):
+        self.__nome = nome
         self.__estamina = estamina
         self.__dinheiro = dinheiro
-        
+
+    def __str__(self) -> str:
+        return f''' Personagem: {self.__nome}
+                    Pontos de estamina: {self.__estamina}
+                    Unidades de Dinheiro: {self.__dinheiro}
+                '''
+
+    @property
+    def statusEstamina(self):
+        return self.__estamina
+
+    @statusEstamina.setter
+    def statusEstaminaAD(self, estamina):
+        self.__estamina += estamina
+
+    @statusEstamina.setter
+    def statusEstaminaRE(self, estamina):
+        self.__estamina -= estamina
--- trabalho.py
+++ trabalho.py
@@ -1,47 +1,57 @@
+from random import randint
+from dev import Dev
+
+
 class Trabalho:
+
     def __init__(self, trabalho):
-        self.trabalho = trabalho
-    
-    def homeOffice(self, dinheiro, estamina):
-        self.__dinheiro += 10 #protegendo o atributo
+        self.__trabalho = trabalho
+
+    def homeOffice(self):  # dinheiro, estamina
+        self.__dinheiro += 10  # protegendo o atributo
         self.__estamina -= 6
 
-    def presencial(self, dinheiro, estamina):
+    def presencial(self):  # dinheiro, estamina
         self.__dinheiro += 10
         self.__estamina -= 8
 
-    def aleatoria(self, dinheiro, estamina):
+    def aleatoria(self):  # dinheiro, estamina
         self.__dinheiro = dinheiro
         self.__estamina = estamina
+
         from random import choice
-        eventos = ('acidente de carro', 'dor de barriga', 'happy hour', 'dor de cabeça', 'quebrou a perna')
+        eventos = ('Acidente de carro', 'Dor de barriga',
+                   'Dappy hour', 'Dor de cabeça', 'Quebrou a perna')
         perde_dia = choice(eventos)
-        if perde_dia == 'acidente de carro':
-            dinheiro -= 2
-            return f'''Você sofreu um acidente voltando do trabalho. Ficará impossibilitado de trabalhar por 2 dias.
-                Você perdeu 2 unidades de dinheiro'''
-            saldo()
-        elif perde_dia == 'dor de barriga':
-            dinheiro -= 1
-            return f'''Você comeu algo estragado. Ficará impossibilitado de trabalhar por 1 dias.
-                Você perdeu 1 unidade de dinheiro'''
-            saldo()
-        elif perde_dia == 'happy hour':
-            opcao = input('Você recebeu um convite para um happy hour. Deseja ir? [S/N]').upper().strip()[0]
-            if opcao == 'S':
-                dinheiro -= 1
-                estamina -= 2
-                return f'''Você exagerou... Chegou tarde em casa e não vai conseguir dormir o número suficiente de horas para recuperar suas energias.'''
-            else:
-                return f'Obrigado... fica para uma próxima vez...'
-        elif perde_dia == 'dor de cabeça':
-            dinheiro -= 1
-            return f'''Você acordou com uma baita dor de cabeça e não vai conseguir trabalhar por 1 dia.
-                Você perdeu 1 unidade de dinheiro'''
-            saldo()
-        elif perde_dia == 'quebrou a perna':
-            dinheiro -= 1
-            return f'''Você quebrou a perna e não vai conseguir trabalhar por 7 dias.
-                Você perdeu 7 unidades de dinheiro'''
-            saldo()
-        
+
+        eventoAleatorio = randint(1, 100)
+        if eventoAleatorio > 90:
+            if perde_dia == 'acidente de carro':
+                self.__dinheiro -= 10
+                return f'''Você sofreu um acidente voltando do trabalho. Ficará impossibilitado de trabalhar por 2 dias.
+                    Você perdeu 10 unidades de dinheiro'''
+                saldo()
+            elif perde_dia == 'dor de barriga':
+                self.__dinheiro -= 1
+                return f'''Você comeu algo estragado. Ficará impossibilitado de trabalhar por 1 dias.
+                    Você perdeu 5 unidade de dinheiro'''
+                saldo()
+            elif perde_dia == 'happy hour':
+                opcao = input(
+                    'Você recebeu um convite para um happy hour. Deseja ir? [S/N]').upper().strip()[0]
+                if opcao == 'S':
+                    self.__dinheiro -= 1
+                    self.__estamina -= 2
+                    return f'''Você exagerou... Chegou tarde em casa e não vai conseguir dormir o número suficiente de horas para recuperar suas energias.'''
+                else:
+                    return f'Obrigado... fica para uma próxima vez...'
+            elif perde_dia == 'dor de cabeça':
+                self.__dinheiro -= 1
+                return f'''Você acordou com uma baita dor de cabeça e não vai conseguir trabalhar por 1 dia.
+                    Você perdeu 1 unidade de dinheiro'''
+                saldo()
+            elif perde_dia == 'quebrou a perna':
+                self.__dinheiro -= 1
+                return f'''Você quebrou a perna e não vai conseguir trabalhar por 7 dias.
+                    Você perdeu 7 unidades de dinheiro'''
+                saldo()
